8fae3dd14643a0c3c7392ccce236f2dad3d7289d
anorico <allan.norico@gmail.com>
2016-03-28 10:15:09 -0400

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,20 @@
 # Enter your procedural solution here!
+require 'prime' 
+
+def largest_prime_factor(prime_number)
+  i = 1
+  array = []
+  best_number = prime_number
+  prime_left = prime_number
+
+  while i <= best_number
+    p "original: #{prime_number} | current: #{best_number} | LPF testing: #{i} | array: #{array}"
+   if best_number % i == 0 && Prime.prime?(i) == true
+      array << i
+      best_number = best_number/array[-1]
+   end
+   i += 2
+  end
+  return array.max 
+end
+
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,33 @@
 # Enter your object-oriented solution here!
+require 'prime' 
+
+class LargestPrimeFactor
+
+  def initialize(prime_number)
+
+    @array = Array.new
+    best_number = prime_number 
+    prime_left = prime_number
+
+    i=1
+
+    while i <= best_number
+
+      p "original: #{prime_number} | current: #{best_number} | LPF testing: #{i} | @array: #{@array}"
+
+      if best_number % i == 0 && Prime.prime?(i) == true
+        @array << i
+        best_number = best_number/@array[-1]
+      end
+
+      i += 2
+    end
+    return @array
+  end
+
+  def number
+    return @array.max
+  end
+
+end
+
